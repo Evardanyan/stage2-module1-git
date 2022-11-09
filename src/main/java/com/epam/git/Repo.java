e644d2a961bc0805ebafc7ce6dc02621c3b3c652
Evardanyan <64473803+Evardanyan@users.noreply.github.com>
2022-11-10 02:09:50 +0400

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/Evardanyan/mjc_git.git";
+    public static String REPO_LINK = "https://github.com/Evardanyan/git_task.git";
 }
